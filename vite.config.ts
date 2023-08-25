78d8b80d9493a95235f8383c2230bc3051077021
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-08-25 23:49:17 +0900

    chore: add tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { whyframe } from '@whyframe/core';
 import { whyframeSvelte } from '@whyframe/svelte';
+import postcss from './postcss.config.js';
 
 export default defineConfig({
-    plugins: [
-        whyframe(),
-        whyframeSvelte(),
-        sveltekit(),
-    ]
+  plugins: [
+    whyframe(),
+    whyframeSvelte(),
+    sveltekit(),
+  ],
+  css: {
+    postcss
+  }
 });
